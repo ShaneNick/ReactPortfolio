ab0fd9aba9ac4a2858d6e6d793ca67ad7fde6a47
ShaneNick <shanetomonia@gmail.com>
2023-07-19 15:47:09 -0400

    added contact and final details

--- portfolio/src/pages/ProjectPage.js
+++ portfolio/src/pages/ProjectPage.js
@@ -7,14 +7,27 @@ import "../styles/ProjectDisplay.css";
 function ProjectPage() {
   const { id } = useParams();
   const project = ProjectList[id];
+
   return (
     <div className="project">
-      <h1> {project.name}</h1>
+      <h1>{project.name}</h1>
       <img src={project.image} alt={project.name} />
       <p>
         <b>Skills:</b> {project.skills}
       </p>
-      <GitHubIcon />
+      <div className="projectLinks">
+        <div className="githubLink">
+          <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+            <GitHubIcon className="githubIcon" />
+          </a>
+          <span>GitHub</span>
+        </div>
+        {project.deployedLink && (
+          <a href={project.deployedLink} target="_blank" rel="noopener noreferrer">
+            <button>View Project</button>
+          </a>
+        )}
+      </div>
     </div>
   );
 }
